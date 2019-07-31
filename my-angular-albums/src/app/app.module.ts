e7304942ecea9409f7df465ff1deb1d3e987ea15
Tom P <johndoe@example.com>
2019-07-31 11:03:01 -0400

    next update

--- my-angular-albums/src/app/app.module.ts
+++ my-angular-albums/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AlbumListComponent } from './albums/album-list/album-list.component';
+import { MyComponentComponent } from './test/my-component/my-component.component';
+import { AlbumCardComponent } from './albums/album-card/album-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlbumListComponent,
+    MyComponentComponent,
+    AlbumCardComponent
   ],
   imports: [
     BrowserModule,
